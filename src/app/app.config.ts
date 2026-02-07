53f8b82e229c7c2bfa0d1e04d0fd2291c7968f1e
Yash Wakte <yashwakte48@gmail.com>
2026-02-08 01:20:16 +0530

    refactor: Simplify app configuration by removing unused router and hydration providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withHashLocation()),
-    provideClientHydration(withEventReplay()),
-  ],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };
